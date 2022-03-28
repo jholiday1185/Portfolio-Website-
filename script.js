028e5be3af0a7606f9fe54f8b825f6260505a046
Justin Holiday <jus1185@yahoo.com>
2022-03-28 12:53:51 -0400

    Add about-contact page

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 
-// Set the width of the side navigation to 250px
+function showNavDrop() {
 
-function openNav() {
-
-    document.getElementById("mySidenav").style.width = "250px"; 
+        document.getElementById("project-dropdown").classList.toggle("show");
 }
 
-// Set the width of the side navigation to 0
 
-function closeNav() {
-    
-    document.getElementById("mySidenav").style.width = "0";
+window.onclick = function(e) {
+    if (!e.target.matches(".dropbtn")) {
+        var myDropdown = document.getElementById("project-dropdown");
+        if (myDropdown.classList.contains("show")) {
+            myDropdown.classList.remove("show");
+                }
+    }
 }
 
+
+function smartSearch() {
+    // Dedlare variables 
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("mySearch");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("my-projects");
+    li = document.getElementsByTagName("li");
+
+    //loop through list items and hide items that dont match query
+    for(i=0; i < li.length; i++ ) {
+
+        a = li[i].getElementsByTagName("a")[0];
+        if(a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        }else {
+            li[i].style.display = "none";
+        }
+    }
+}
